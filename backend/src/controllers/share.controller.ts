4f76eac40170da191c7cc286ca40882d7838a000
TanishqJangir <tanishqjangir001@gmail.com>
2026-03-17 02:23:53 +0530

    updated the tags controller

--- backend/src/controllers/share.controller.ts
+++ backend/src/controllers/share.controller.ts
@@ -5,75 +5,75 @@ import { Vault } from '../models/Vault.model';
 import crypto from 'crypto';
 
 
-export const createShareLinkController = async (req: Request, res: Response) : Promise<any> => {
-    try{
+export const createShareLinkController = async (req: Request, res: Response): Promise<any> => {
+    try {
         const userId = (req.user as TokenPayload).userId;
 
         const shareHash = crypto.randomBytes(12).toString("hex"); // 24 char random string
         const shareLinkExpiry = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
 
         const user = await User.findByIdAndUpdate(userId, {
-
-            $set:{
-            shareLink : shareHash,
-            shareLinkExpiry: shareLinkExpiry,
+            $set: {
+                shareLink: shareHash,
+                shareLinkExpiry: shareLinkExpiry,
             },
-        },{
+        }, {
             new: true,
         });
 
-        if(!user){
+        if (!user) {
             return res.status(400).json({
-                message : "User does not exist.",
+                message: "User does not exist.",
             });
         };
 
         return res.status(200).json({
-            message : "Share link created successfully.",
-            shareLink : shareHash,
+            message: "Share link created successfully.",
+            shareLink: shareHash,
         });
 
-    }catch(error){
+    } catch (error) {
         console.error("Error while creating share link: ", error);
         return res.status(500).json({
-            message : "Internal Server Error."
+            message: "Internal Server Error."
         });
     };
 };
 
-export const getSharedLinkController = async (req: Request, res: Response) : Promise<any> => {
-    try{
+export const getSharedLinkController = async (req: Request, res: Response): Promise<any> => {
+    try {
         const { shareLink } = req.params;
 
-        if(!shareLink){
+        if (!shareLink) {
             return res.status(400).json({
-                message : "Share link is required.",
+                message: "Share link is required.",
             });
         };
-        
+
         const user = await User.findOne({
-            shareLink : shareLink,
-            shareLinkExpiry : {$gt: new Date()}
+            shareLink: shareLink,
+            shareLinkExpiry: { $gt: new Date() }
         }).select("name avatar _id");
 
-        if(!user){
+        if (!user) {
             return res.status(404).json({
-                message : "Share link not found or expired",
+                message: "Share link not found or expired",
             });
         };
 
-        const contents = await Vault.find({ userId : user._id}).sort({createdAt : -1});
+        const contents = await Vault.find({ userId: user._id }).sort({ createdAt: -1 });
 
         return res.status(200).json({
-            message : "Content fetched successfully.",
+            message: "Content fetched successfully.",
             name: user.name,
-            avatar : user.avatar,
+            avatar: user.avatar,
             contents
         });
-    }catch(error){
-        console.error("Error while fething the contents from the share link: ",error);
+
+    } catch (error) {
+        console.error("Error while fething the contents from the share link: ", error);
         return res.status(500).json({
-            message : "Internal Server Error",
+            message: "Internal Server Error",
         });
     };
 };
--- backend/src/models/Otp.model.ts
+++ backend/src/models/Otp.model.ts
@@ -1,12 +1,12 @@
 import mongoose, { Schema } from "mongoose";
 
 const OtpSchema = new Schema({
-    otp : {type : String, required : true},
-    email : {type : String, required : true},
-    otpExpiry : {type : Date, required : true},
-    isVerified: {type : Boolean, default : false},
+    otp: { type: String, required: true },
+    email: { type: String, required: true },
+    otpExpiry: { type: Date, required: true },
+    isVerified: { type: Boolean, default: false },
 }, {
-    timestamps : true    
+    timestamps: true
 })
 
 OtpSchema.index({ otpExpiry: 1 }, { expireAfterSeconds: 0 });
